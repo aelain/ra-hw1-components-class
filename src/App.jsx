703f35b8ff7076322882bffa86500f9d27e42138
Diana Avedisyan <diana-avedisyan@mail.ru>
2024-12-28 14:12:07 +0500

    Update App.jsx and ShopItemClass.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,22 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="container">
-      <div className="background-element">
+        <div className="background-element">
+        </div>
+        <div className="highlight-window">
+          <div className='highlight-overlay'></div>
+        </div>
+        <div className="window">
+          <ShopItemClass
+            brand="Tiger of Sweden"
+            title="Leonard coat"
+            description="Minimalistic coat in cotton-blend"
+            descriptionFull="Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length."
+            price="399"
+            currency="£"
+          />
+        </div>
       </div>
-      <div className="highlight-window">
-        <div className='highlight-overlay'></div>
-      </div>
-      <div className="window">
-        <ShopItemClass />
-      </div>
-    </div>
     );
   }
 }
--- src/components/ShopItemClass.jsx
+++ src/components/ShopItemClass.jsx
@@ -1,34 +1,32 @@
 import React from 'react';
-import Item from '../classes/Item';
 
 export default class ShopItemClass extends React.Component {
-  render() {
-    const item = new Item(
-      'Tiger of Sweden',
-      'Leonard coat',
-      'Minimalistic coat in cotton-blend',
-      'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-      399,
-      '£'
-    );
+  constructor(item) {
+    super(item);
 
-    if (item instanceof Item) {
-      return (
-        <div className="main-content">
-          <h2>{item.brand}</h2>
-          <h1>{item.title}</h1>
-          <h3>{item.description}</h3>
-          <div className="description">
-            {item.descriptionFull}
-          </div>
-          <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
-          <div className="divider"></div>
-          <div className="purchase-info">
-            <div className="price">{item.currency}{item.price.toFixed(2)}</div>
-            <button>Добавить в корзину</button>
-          </div>
+    this.brand = item.brand;
+    this.title = item.title;
+    this.description = item.description;
+    this.descriptionFull = item.descriptionFull;
+    this.price = item.price;
+    this.currency = item.currency;
+  }
+  render() {
+    return (
+      <div className="main-content">
+        <h2>{this.brand}</h2>
+        <h1>{this.title}</h1>
+        <h3>{this.description}</h3>
+        <div className="description">
+          {this.descriptionFull}
         </div>
-      );
-    }
+        <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+        <div className="divider"></div>
+        <div className="purchase-info">
+          <div className="price">{this.currency}{Number(this.price).toFixed(2)}</div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+    );
   }
 }
